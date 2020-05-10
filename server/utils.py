a494906541220cd0cf34aa444ec595eef941551b
John Keck <robertjkeck2@gmail.com>
2020-05-09 22:33:18 -0400

    added initialization code for datasets

--- server/utils.py
+++ server/utils.py
@@ -2,6 +2,7 @@ import os
 
 import librosa
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 from config import (
     RAVDESS_FILENAME_FORMAT,
@@ -61,3 +62,87 @@ def parse_savee_filename(filename):
         "filepath": str(SAVEE_FILEPATH / filename),
         "label": sample["label"],
     }
+
+
+def prepare_data(files, data_set, num_coefs):
+    X = []
+    y = []
+    num_files = len(files)
+    for i, f in enumerate(files):
+        parsed_file = FILENAME_PARSERS[data_set](f)
+        mfc_coefs = calculate_mel_frequency_cepstral_coefficients(
+            parsed_file.get("filepath"), num_coefs)
+        X.append(mfc_coefs)
+        y.append(parsed_file.get("label"))
+    return X, y
+
+
+def initialize_ravdess_savee_data(num_coefs):
+    savee_files = [x for x in os.listdir(
+        SAVEE_FILEPATH) if fnmatch.fnmatch(x, '*.wav')]
+    ravdess_files = []
+
+    for actor in os.listdir(RAVDESS_FILEPATH):
+        if actor not in (".DS_Store", "README.md"):
+            for f in os.listdir(RAVDESS_FILEPATH / actor):
+                ravdess_files.append(f)
+
+    ravdess_X, ravdess_y = prepare_data(ravdess_files, "RAVDESS", num_coefs)
+    savee_X, savee_y = prepare_data(savee_files, "SAVEE", num_coefs)
+
+    X = np.asarray(ravdess_X + savee_X)
+    y = np.asarray(ravdess_y + savee_y)
+
+    np.savetxt(
+        "/data/RAVDESS+SAVEE_MFCC/X_{}.csv".format(num_coefs), X, delimiter=",")
+    np.savetxt(
+        "/data/RAVDESS+SAVEE_MFCC/y_{}.csv".format(num_coefs), y, delimiter=",")
+
+
+def initialize_ravdess_data(num_coefs):
+    ravdess_files = []
+
+    for actor in os.listdir(RAVDESS_FILEPATH):
+        if actor not in (".DS_Store", "README.md"):
+            for f in os.listdir(RAVDESS_FILEPATH / actor):
+                ravdess_files.append(f)
+
+    ravdess_X, ravdess_y = prepare_data(ravdess_files, "RAVDESS", num_coefs)
+
+    X = np.asarray(ravdess_X)
+    y = np.asarray(ravdess_y)
+
+    np.savetxt(
+        "/data/RAVDESS_MFCC/X_{}.csv".format(num_coefs), X, delimiter=",")
+    np.savetxt(
+        "/data/RAVDESS_MFCC/y_{}.csv".format(num_coefs), y, delimiter=",")
+
+
+def initialize_savee_data(num_coefs):
+    savee_files = os.listdir(SAVEE_FILEPATH)
+    savee_X, savee_y = prepare_data(savee_files, "SAVEE", num_coefs)
+    np.savetxt("/data/SAVEE_MFCC/X_{}.csv".format(num_coefs),
+               savee_X, delimiter=",")
+    np.savetxt("/data/SAVEE_MFCC/y_{}.csv".format(num_coefs),
+               savee_y, delimiter=",")
+
+
+def load_dataset(num_coefs, train_pct, dataset="RAVDESS"):
+    X = np.loadtxt(
+        f"/data/{dataset}_MFCC/X.csv", delimiter=',')
+    y = np.loadtxt(
+        f"/data/{dataset}_MFCC/y.csv", delimiter=',')
+
+    if train_pct is not None:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, train_size=train_pct)
+        X_train = np.expand_dims(X_train, axis=2)
+        X_test = np.expand_dims(X_test, axis=2)
+    else:
+        X_train, X_test, y_train, y_test = X, np.array([]), y, np.array([])
+        X_train = np.expand_dims(X_train, axis=2)
+
+    return X_train, X_test, y_train, y_test
+
+
+initialize_savee_data(40)
