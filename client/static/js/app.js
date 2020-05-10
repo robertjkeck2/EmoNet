e235e96d81e1459620da4cfe1e9538575aa8d88e
John Keck <robertjkeck2@gmail.com>
2020-05-09 21:20:52 -0400

    added temp wave file and styling

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -1,89 +1,42 @@
-//webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 
-var gumStream; //stream from getUserMedia()
-var rec; //Recorder.js object
-var input; //MediaStreamAudioSourceNode we'll be recording
-
-// shim for AudioContext when it's not avb.
+var gumStream;
+var rec;
+var input;
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext; //audio context to help us record
-
+var audioContext;
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 
-//add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 
 function startRecording() {
-  /*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-
   var constraints = { audio: true, video: false };
-
-  /*
-    	Disable the record button until we get a success or fail from getUserMedia() 
-	*/
-
   recordButton.disabled = true;
   stopButton.disabled = false;
-
-  /*
-    	We're using the standard promise based getUserMedia() 
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function (stream) {
-      /*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-		*/
       audioContext = new AudioContext();
-
-      //update the format
       document.getElementById("formats").innerHTML =
         "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz";
-
-      /*  assign to gumStream for later use  */
       gumStream = stream;
-
-      /* use the stream */
       input = audioContext.createMediaStreamSource(stream);
-
-      /* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
       rec = new Recorder(input, { numChannels: 1 });
-
-      //start the recording process
       rec.record();
     })
     .catch(function (err) {
-      //enable the record button if getUserMedia() fails
       recordButton.disabled = false;
       stopButton.disabled = true;
     });
 }
 
 function stopRecording() {
-  //disable the stop button, enable the record too allow for new recordings
   stopButton.disabled = true;
   recordButton.disabled = false;
-
-  //tell the recorder to stop the recording
   rec.stop();
-
-  //stop microphone access
   gumStream.getAudioTracks()[0].stop();
-
-  //create the wav blob and pass it on to createDownloadLink
   rec.exportWAV(createDownloadLink);
 }
 
@@ -91,34 +44,23 @@ function createDownloadLink(blob) {
   var url = URL.createObjectURL(blob);
   var au = document.createElement("audio");
   var li = document.createElement("li");
-
-  //name of .wav file to use during upload and download (without extendion)
   var filename = new Date().toISOString();
-
-  //add controls to the <audio> element
   au.controls = true;
   au.src = url;
-
-  //add the new audio element to li
   li.appendChild(au);
 
-  //add the filename to the li
-  li.appendChild(document.createTextNode(filename + ".wav "));
-
-  //upload link
   var upload = document.createElement("a");
   upload.href = "#";
-  upload.innerHTML = "Upload";
+  upload.innerHTML = "<button>Submit</button>";
   upload.addEventListener("click", function (event) {
     var xhr = new XMLHttpRequest();
     var fd = new FormData();
     fd.append("audio_data", blob, filename);
     xhr.open("POST", "/api/v1/process-audio", true);
     xhr.send(fd);
+    recordingsList.innerHTML = "";
   });
-  li.appendChild(document.createTextNode(" ")); //add a space in between
-  li.appendChild(upload); //add the upload link to li
-
-  //add the li element to the ol
+  li.appendChild(upload);
+  recordingsList.innerHTML = "";
   recordingsList.appendChild(li);
 }
